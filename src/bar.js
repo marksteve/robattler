cc6977ac6ce71e92e3c0eb20539bc0ffc239ee04
Mark Steve Samson <hello@marksteve.com>
2014-12-08 09:50:45 +0800

    Make bars smaller

--- src/bar.js
+++ src/bar.js
@@ -1,16 +1,18 @@
 var Bar = function(game, robot, type, right) {
   var x = right ? game.world.bounds.right - 30 : 30;
-  var y = type == 'hp' ? 20 : 60;
-  Phaser.TileSprite.call(this, game, x, y, 16, 32, 'bar');
-  this.width = type == 'hp' ? 100 : 60;
+  var y = type == 'hp' ? 20 : 40;
+  Phaser.TileSprite.call(this, game, x, y, 32, 16, 'bar');
+  this.width = type == 'hp' ? 100 : 40;
+  this.type = type;
+  this.robot = robot;
 
   this.fill = new Phaser.TileSprite(
-    this.game, 0, 0, 32, 32, type);
+    game, 0, 0, 16, 16, type);
   this.fill.alpha = 0.4;
-  this.fill.width = this.width;
+  this.fill.width = 0;
   this.addChild(this.fill);
 
-  this.braces = this.game.add.group(this);
+  this.braces = game.add.group(this);
   this.braces.create(
     -16, 0, 'brace'
   );
@@ -19,13 +21,17 @@ var Bar = function(game, robot, type, right) {
   ).scale.x *= -1;
 
   if (right) {
-    this.anchor.set(1, 0);
+    this.anchor.x = 1;
+    this.fill.anchor.x = 1;
     this.braces.x -= this.width;
-    this.fill.x -= this.width;
   }
 };
 
 Bar.prototype = Object.create(Phaser.TileSprite.prototype);
 Bar.prototype.constructor = Bar;
+Bar.prototype.update = function() {
+  var max = this.type == 'hp' ? 100 : 15;
+  this.fill.width = this.width * this.robot[this.type] / max;
+};
 
 module.exports = Bar;
